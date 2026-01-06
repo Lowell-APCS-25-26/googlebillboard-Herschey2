ca5289d6a019be0cbfb76c5c28c66f4b1a5f9f19
Herschey2 <96698273+Herschey2@users.noreply.github.com>
2026-01-06 08:28:45 -0800

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,7 +3,7 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
-		System.out.println(final);
+		System.out.println("Not the right string, tests will fail!");
 	}
 
 }
